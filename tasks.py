43099994f33d33592728114ac42ea9057d20c9b5
shravanthirn97-wq <shravanthirn97@gmail.com>
2026-04-06 20:56:05 +0530

    fix models duplicate issue

--- tasks.py
+++ tasks.py
@@ -14,19 +14,3 @@ tasks_list = [
         {"name": "Movie", "priority": 1, "deadline": 5, "duration": 2}
     ]
 ]
-tasks_list = [
-
-    [
-        {"name": "Assignment", "priority": 3},
-        {"name": "Gaming", "priority": 1}
-    ],
-    [
-        {"name": "Exam Study", "priority": 3, "deadline": 1},
-        {"name": "Gym", "priority": 2, "deadline": 3}
-    ],
-
-    [
-        {"name": "Project", "priority": 3, "deadline": 1, "duration": 5},
-        {"name": "Movie", "priority": 1, "deadline": 5, "duration": 2}
-    ]
-]
